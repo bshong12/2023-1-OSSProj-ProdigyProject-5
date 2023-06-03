c589edd7b6cb6337cad0f727b326a136c5b31b1d
bshong12 <bshong3593@dgu.ac.kr>
2023-06-03 21:17:03 +0900

    [23.06.03 홍범선](Mod) 로그인,회원가입 db연동

--- Back-End/controllers/login.js
+++ Back-End/controllers/login.js
@@ -1,40 +1,36 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
-const bodyParser = require('body-parser');
-router.use(express.json());
-router.use(bodyParser.urlencoded({ extended: false }));
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-
+const db = require('../DB/db');
 router.post('/', logIn);
-
-const users = [{"id":"1234", "name":"qwe", "email":"asd" },{"id":"2468", "name":"asd", "email":"qwe" }]
 
 async function logIn(req, res){
   try{
     const {id, password} = req.body;
 
     // 사용자 찾기
-    //users -> db에서 가져오기
+    const users = await db.getUser(); // users는 데이터베이스에서 가져옴
     const user = users.find(user => user.id === id);
     if (!user) {
       throw new Error('사용자를 찾을 수 없습니다.');
     }
 
     // 비밀번호 검증
-    // const passwordMatch = await bcrypt.compare(password, user.password);
-    // if (!passwordMatch) {
-    //   throw new Error('비밀번호가 일치하지 않습니다.');
-    // }
-    // else{
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      throw new Error('비밀번호가 일치하지 않습니다.');
+    }
+    else{
       
       // access token발급
       const accessToken = jwt.sign({
         id : user.id,
         username: user.name,
-        phone : user.phonenumber,
-        email : user.email
+        phone : user.phone,
+        email : user.email,
+        type : user.type
       }, process.env.ACCESS_SECRET,{
         expiresIn : '1m'
       })
@@ -53,12 +49,17 @@ async function logIn(req, res){
         secure : false,
         httpOnly : true
       })
-
-      res.status(200).json('login success')
+      if(user.type == 'S'){
+        res.status(200).json({type:'학생'})
+      }
+      else {
+        res.status(200).json({type:"관리자"})
+      }
+      
       
     }
 
-  // }
+  }
   
   catch(err){
     res.status(500).json({ error: err.message });
